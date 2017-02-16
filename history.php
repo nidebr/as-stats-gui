32cf279b92f9dc3943265607f25c824d35f3f52c
Nicolas Debrigode <n.debrigode@hexanet.fr>
2017-02-16 09:35:41 +0100

    auto-refresh history page

--- history.php
+++ history.php
@@ -51,6 +51,7 @@ if ( isset($_GET['as']) ) {
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta http-equiv="Refresh" content="300">
   <title><?php echo $title ?></title>
   <link rel="icon" href="favicon.ico" />
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
