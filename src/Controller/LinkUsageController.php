72763ca69109f43bb3b1b6da698e04a9972bbaad
Nicolas Debrigode <n.debrigode@hexanet.fr>
2024-01-29 17:09:54 +0100

    save

---
+++ src/Controller/LinkUsageController.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Util\Annotation\Menu;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route(
+    path: '/links/usage',
+)]
+#[Menu('links_usage')]
+class LinkUsageController extends BaseController
+{
+    protected array $data = [];
+
+    #[Route(
+        path: '/',
+        name: 'links.usage',
+        methods: ['GET'],
+    )]
+    public function index(): Response
+    {
+        return $this->render('pages/link_usage/index.html.twig', [
+            'base_data' => $this->base_data,
+        ]);
+    }
+
+    #[Route(
+        path: '/{topinterval}',
+        name: 'links.usage.topinterval',
+        methods: ['GET'],
+    )]
+    public function indexTopInterval(
+        string $topinterval,
+    ): Response {
+        return $this->render('pages/link_usage/index.html.twig', [
+            'base_data' => $this->base_data,
+        ]);
+    }
+}
